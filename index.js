3f96a0f8dbcc24ca4f82e3cadefbbbec8415f8eb
grayTurtle01 <the.correo@protonmail.com>
2023-07-19 13:17:57 -0600

    linter fixed

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+  res.send('Hello Express')
 })
 
-
 app.listen(8000, () => {
-    console.log('Listen on port 8000')
+  console.log('Listen on port 8000')
 })

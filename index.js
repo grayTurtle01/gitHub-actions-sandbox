c1af9a06f9752b8f59ea26637b34a606fa3a3674
grayTurtle01 <the.correo@protonmail.com>
2023-07-19 12:43:26 -0600

    express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello Express')
+})
+
+
+app.listen(8000, () => {
+    console.log('Listen on port 8000')
+})
